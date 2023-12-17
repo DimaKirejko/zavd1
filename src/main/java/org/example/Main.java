81a6253004ac88161c3365849a19fe9635fc7e53
DimaKirejko <iwerlandowawww1@gmail.com>
2023-12-17 16:15:02 +0200

    commit for 1 task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,62 +1,86 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
-    public enum SortingType{
-        BUBBLE,
-        SHELL,
-        MERGE,
-        QUICK
-    }
-    public static void fill(ArrayList<Integer> list, int size) {
-        Random rand = new Random();
-        for (int i = 0; i < size; i++) {
-            list.add(rand.nextInt(size));
-        }
-    }
 
-    public static void sortingTime(ArrayList<Integer> input, SortingType type, long startTime, long currentTime){
-        System.out.printf("%s spent %d milliseconds\n",type.toString().toLowerCase() , currentTime - startTime);
-    }
-
-    public static Sorted createSorter(SortingType type){
-        Sorted sorter = null;
-        switch (type) {
-            case BUBBLE:
-                sorter = new BubbleSorter();
-                break;
-            case SHELL:
-                sorter = new ShellSorter();
-                break;
-            case MERGE:
-                sorter = new MergeSorter();
-                break;
-            case QUICK:
-                sorter = new QuickSorter();
-                break;
-        }
-        return sorter;
-    }
-
+    private static final int LIST_SIZE = 100_000;
+    private static final int INSERT_COUNT = 1_000;
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        int[] counts = {10, 1_000, 10_000, 1_000_000};
-        for (int count : counts) {
-            System.out.println("\nSorting list with " + count + " elements");
-            fill(arrayList, count);
-            for (SortingType type: SortingType.values()) {
-                long time = System.currentTimeMillis();
-                ArrayList<Integer> outputList;
-                Sorted sorted = createSorter(type);
-                outputList = sorted.sort(new ArrayList<>(arrayList));
-                for (int i = 0; i < 50 && i < outputList.size(); i++) {
-                    System.out.print(outputList.get(i) + ",");
-                }
-                sortingTime(outputList, type, time, System.currentTimeMillis());
-            }
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        fillList(arrayList, LIST_SIZE, "ArrayList");
+        fillList(linkedList, LIST_SIZE, "LinkedList");
+
+        testRandomAccess(arrayList, "ArrayList");
+        testRandomAccess(linkedList, "LinkedList");
+
+        testSequentialAccess(arrayList, "ArrayList");
+        testSequentialAccess(linkedList, "LinkedList");
+
+        testInsertAtStart(arrayList, INSERT_COUNT, "ArrayList");
+        testInsertAtStart(linkedList, INSERT_COUNT, "LinkedList");
+
+        testInsertAtEnd(arrayList, INSERT_COUNT, "ArrayList");
+        testInsertAtEnd(linkedList, INSERT_COUNT, "LinkedList");
+
+        testInsertAtMiddle(arrayList, INSERT_COUNT, "ArrayList");
+        testInsertAtMiddle(linkedList, INSERT_COUNT, "LinkedList");
+    }
+
+    private static void fillList(List<Integer> list, int count, String listType) {
+        long startTime = System.currentTimeMillis();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            list.add(random.nextInt(count));
         }
+        printExecutionTime("Fill " + listType, startTime);
+    }
+
+    private static void testRandomAccess(List<Integer> list, String listType) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < list.size(); i++) {
+            int element = list.get(i);
+        }
+        printExecutionTime("Random access in " + listType, startTime);
+    }
+
+    private static void testSequentialAccess(List<Integer> list, String listType) {
+        long startTime = System.currentTimeMillis();
+        for (Integer ignored : list) {
+            // Performing some operation to prevent optimization
+        }
+        printExecutionTime("Sequential access in " + listType, startTime);
+    }
+
+    private static void testInsertAtStart(List<Integer> list, int count, String listType) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            list.add(0, i);
+        }
+        printExecutionTime("Insert at start of " + listType, startTime);
+    }
+
+    private static void testInsertAtEnd(List<Integer> list, int count, String listType) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            list.add(i);
+        }
+        printExecutionTime("Insert at end of " + listType, startTime);
+    }
+
+    private static void testInsertAtMiddle(List<Integer> list, int count, String listType) {
+        long startTime = System.currentTimeMillis();
+        int middleIndex = list.size() / 2;
+        for (int i = 0; i < count; i++) {
+            list.add(middleIndex, i);
+        }
+        printExecutionTime("Insert in the middle of " + listType, startTime);
+    }
+//
+    private static void printExecutionTime(String action, long startTime) {
+        System.out.printf("%s: %s(ms)\n", action, System.currentTimeMillis() - startTime);
     }
 }
--- src/main/java/org/example/Sorted.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-import java.util.ArrayList;
-
-public interface Sorted
-{
-    public ArrayList<Integer> sort(ArrayList<Integer> input);
-}
